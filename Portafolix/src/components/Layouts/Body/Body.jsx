a4a101a76e22516880fc583b476efd9124840dd7
Sena <dimatesebas73@gmail.com>
2025-03-21 11:36:30 -0500

    advance

--- Portafolix/src/components/Layouts/Body/Body.jsx
+++ Portafolix/src/components/Layouts/Body/Body.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
-import { Carousel } from "../Carousel/Carousel";
+
 
 export const Body = () => {
   return (
-    <Carousel/>
+    <>
+    <div className='body'>
+    <h1>¡Bienvenido a mi portafolio!</h1>
+
+    <p>Soy Juan, desarrollador de software con experiencia en Java, <br />
+       PHP y JavaScript. Me especializo en crear soluciones eficientes y escalables,<br />
+        aplicando las mejores prácticas de desarrollo para garantizar código limpio y funcional. <br />
+         En mi portafolio, encontrarás proyectos que reflejan mi capacidad para <br />
+          construir aplicaciones robustas y optimizar procesos. <br />
+          ¡Explora mi trabajo y descubre cómo puedo aportar valor a tu próximo proyecto!</p>
+
+          <img src="./src/assets/imgs/IMG_3041.webp" alt="IMG-1" />
+          </div>
+    </>
   )
 }
